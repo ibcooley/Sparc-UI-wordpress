8b36059e5721857a9e3ac80aa146fce0a90fbdf3
shivika <shivika.asthana@sparcedge.com>
2014-05-05 12:46:44 -0400

    spacing, updating gitignore, added main content section to templates, added page title to headerwHero template

--- sparcwp/page-full-width.php
+++ sparcwp/page-full-width.php
@@ -7,12 +7,19 @@
  */
 
 get_header(); ?>
-
-
+	<section class="main-content">
+		<div class="container">
+		<!-- .entry-header -->
+		        <header class="page-header">
+				<h1 class="page-title"><?php the_title(); ?></h1>
+			</header>
+		</div>
+		
 			<?php while ( have_posts() ) : the_post(); ?>
 			
 				<?php the_content(); ?>
 
 			<?php endwhile; // end of the loop. ?>
+	</section>
 
 <?php get_footer(); ?>
--- sparcwp/page-headerWithHero.php
+++ sparcwp/page-headerWithHero.php
@@ -7,11 +7,17 @@
  */
 
 get_header('withHero'); ?>
-
+	<section class="main-content">
+		<div class="container">
+		<!-- .entry-header -->
+		        <header class="page-header">
+				<h1 class="page-title"><?php the_title(); ?></h1>
+			</header>
+		</div>
 			<?php while ( have_posts() ) : the_post(); ?>
 			
 				<?php the_content(); ?>
 
 			<?php endwhile; // end of the loop. ?>
-
+	</section>
 <?php get_footer(); ?>
